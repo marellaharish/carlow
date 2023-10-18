c21f793cc48e8829b0a46da0a1b95fd47706beb5
Harish <mharish@pandorarndlabs.com>
2023-10-18 18:24:54 +0530

    Page Update

--- src/Views/Pages/Version1.js
+++ src/Views/Pages/Version1.js
@@ -1,8 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import NavBar from "../../Components/Header/NavBar"
 import Footer from "../../Components/Footer/Footer"
-import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit"
+import { MDBCol, MDBContainer, MDBInput, MDBRow } from "mdb-react-ui-kit"
 function Version1() {
+    const [name, setName] = useState("");
+    const [value, setvalue] = useState(false);
+    const handleBlur = () => {
+        setvalue(true);
+    };
+    const [name1, setName1] = useState("");
+    const [value1, setvalue1] = useState(false);
+    const handleBlur1 = () => {
+        setvalue1(true);
+    };
     return (
         <React.Fragment>
             <>
@@ -11,7 +21,7 @@ function Version1() {
 
                 <MDBContainer fluid>
 
-                    <header className="tux-c-page-header" data-component="Page Header">
+                    <header className="tux-c-page-header">
                         <div className="tux-c-page-header__wrapper tux-l-wrap--md">
 
                             <h1 className="tux-c-page-header__heading">
@@ -23,12 +33,69 @@ function Version1() {
                     <MDBRow>
                         <MDBCol size={7} className="p-0">
                             <div className="home__bg">
+                                <div className="head-and-txt">
 
+                                    <div class="hero-b__heading">
+                                        <h2>
+                                            Start Your Online Application Today
+                                        </h2>
+                                    </div>
+
+                                    <div class="hero-b__content">
+
+                                        <p>We’re excited you’ve considered Carlow University as the next step on your educational journey, and we’re here to help. Whether you’re a first-year, transfer or military-affiliated student, the application process is streamlined so you’ll be on your way to earning a degree in no time. Explore the admissions hub to make sure you have all the required information you need to get started.</p>
+
+                                    </div>
+
+
+                                </div>
                             </div>
                         </MDBCol>
                         <MDBCol size={5} className="p-0">
                             <div className="home__register">
+                                <div className="home-form">
+                                    <h2>Learn More Today</h2>
+                                    <p>Complete this form to learn more about building your future with an online degree from Carlow University.</p>
 
+                                    <form>
+                                        <label>SELECT A PROGRAM</label>
+                                        <select className=" mt-2 form-control select-input placeholder-active active">
+                                            <option>select a Program</option>
+                                            <option>a</option>
+                                            <option>a</option>
+                                            <option>a</option>
+                                        </select>
+                                        <MDBRow className="mt-2">
+                                            <MDBCol>
+                                                <label>
+                                                    First Name
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    className="form-control mt-2"
+                                                    placeholder="First Name"
+                                                    onChange={(e) => {
+                                                        setName(e.target.value);
+                                                    }}
+                                                    onBlur={handleBlur}
+                                                />
+                                                {value && name === "" && <p className="m-0 mt-1">This field is required</p>}
+
+
+                                            </MDBCol>
+                                            <MDBCol>
+                                                <label>
+                                                    Last Name
+                                                </label>
+                                                <input type="text" className="form-control mt-2" placeholder="Last Name" onChange={(e) => {
+                                                    setName1(e.target.value);
+                                                }}
+                                                    onBlur={handleBlur1} />
+                                                {value1 && name1 === "" && <p className="m-0 mt-1">This field is required</p>}
+                                            </MDBCol>
+                                        </MDBRow>
+                                    </form>
+                                </div>
                             </div>
                         </MDBCol>
                     </MDBRow>
@@ -38,20 +105,9 @@ function Version1() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
                 <Footer />
             </>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
 export default Version1
