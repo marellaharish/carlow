8ce55e165220cb0409c6e8c93ca159bb769f2772
Harish <mharish@pandorarndlabs.com>
2023-10-27 18:25:37 +0530

    Css Changes

--- src/programs/Versions.js
+++ src/programs/Versions.js
@@ -29,32 +29,32 @@ function Versions() {
             <div className="dsp-flex">
                 <div className="hm-label"><b>Admission</b></div>
                 <div className="dsp-options">
-                    <a href="/addmissionsv1">Addmissions Version 1</a>
+                    <a href="/addmissionsv1"> Version 1</a>
                     <br />
                     <br />
-                    <a href="/addmissionsv2">Addmissions Version 2</a>
+                    <a href="/addmissionsv2"> Version 2</a>
                     <br />
                     <br />
-                    <a href="/addmissionsv3">Addmissions Version 3</a>
+                    <a href="/addmissionsv3"> Version 3</a>
                     <br />
                     <br />
-                    <a href="/addmissionsv4">Addmissions Version 4</a>
+                    <a href="/addmissionsv4"> Version 4</a>
                     <br />
                     <br />
-                    <a href="/addmissionsv5">Addmissions Version 5</a>
+                    <a href="/addmissionsv5"> Version 5</a>
                 </div>
             </div>
 
             <div className="dsp-flex">
                 <div className="hm-label"><b>Admission Mobile</b></div>
                 <div className="dsp-options">
-                    <a href="/addmissionsmobilev1">Addmissions Mobile Version 1</a>
+                    <a href="/addmissionsmobilev1"> Mobile Tab Version 1</a>
                     <br />
                     <br />
-                    <a href="/addmissionsmobilev2">Addmissions Mobile Version 2</a>
+                    <a href="/addmissionsmobilev2"> Mobile Tab Version 2</a>
                     <br />
                     <br />
-                    <a href="/addmissionsmobilev3">Addmissions Mobile Version 3</a>
+                    <a href="/addmissionsmobilev3"> Mobile Tab Version 3</a>
                 </div>
             </div>
         </div>
